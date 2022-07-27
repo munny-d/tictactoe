497174755c47fc5b6e580680227e476e437298f0
Manpreet Dhillon <manpreet.dhillon04@currys.co.uk>
2022-07-27 18:22:30 +0100

    first commit

---
+++ scripts/config.js
@@ -0,0 +1,36 @@
+function openPlayerConfig(event) {
+  editedPlayer = +event.target.dataset.playerid; // + means it will convert the string to a number
+  playerConfigOverlayElement.style.display = 'block';
+  backdropElement.style.display = 'block';
+
+  console.log('edited player: ' + editedPlayer);
+}
+
+function closePlayerConfig() {
+  playerConfigOverlayElement.style.display = 'none';
+  backdropElement.style.display = 'none';
+  formElement.firstElementChild.classList.remove('error');
+  errorsOutputElement.textContent = '';
+  formElement.firstElementChild.lastElementChild.value = ''; // practising DOM structure - resets form input
+}
+
+function savePlayerConfig(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get('playername').trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add('error');
+    errorsOutputElement.textContent = 'Please enter a valid name';
+    return;
+  }
+
+  const updatedPlayerDataElement = document.getElementById(
+    'player-' + editedPlayer + '-data'
+  );
+  updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  closePlayerConfig();
+}
